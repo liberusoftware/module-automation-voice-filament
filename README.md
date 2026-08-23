193afa56d9af9048349638ee7e29525192294c53
Liberu Automation <opensource@liberu.co.uk>
2026-08-23 23:00:28 +0000

    Publish Automation package

---
+++ README.md
@@ -0,0 +1,4 @@
+# Voice Filament
+
+One-to-one Filament 5 plugin for `module-automation-voice`. The host panel attaches it explicitly; authorization and domain rules remain in the core module.
+
---
+++ src/VoiceFilamentServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liberu\Modules\Automation\Voice\Filament;
+
+use Illuminate\Support\ServiceProvider;
+
+final class VoiceFilamentServiceProvider extends ServiceProvider {}
